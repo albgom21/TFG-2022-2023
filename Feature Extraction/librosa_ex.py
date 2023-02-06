6c8d53a5fdea5ae9d9a445f1b69afbd537118ce4
rubenglezortiz <rubgon08@ucm.es>
2023-02-06 19:58:52 +0100

    avances con librosa
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>

--- Feature Extraction/librosa_ex.py
+++ Feature Extraction/librosa_ex.py
@@ -1,7 +1,7 @@
 import librosa
 import numpy as np
 
-filename = './g.mp3'
+filename = './200-BPM.wav'
 y, sr = librosa.load(filename)
 print("El sample rate es: ", sr)
 
@@ -9,6 +9,11 @@ stft = librosa.stft(y)
 frequencies = librosa.fft_frequencies(sr=sr)
 
 print("Las frecuencias presentes en el archivo de audio son:", frequencies)
-
+6
 bpm = librosa.beat.tempo(y=y, sr=sr)
 print("El BPM estimado es:", bpm[0], "beats por minuto.")
+
+# tono = librosa.hz_to_midi(librosa.pitch_tuning(y, sr))
+# nota = librosa.midi_to_note(tono)
+
+# print("El tono principal del archivo de audio es:", nota)
