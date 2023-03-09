538088ce480a704cebe240468a89076a6f6f4e51
rubenglezortiz <rubgon08@ucm.es>
2023-03-09 10:45:36 +0100

    obstáculos random
    Falta meter más casos

--- unity/Assets/Scripts/ObstacleGenerator.cs
+++ unity/Assets/Scripts/ObstacleGenerator.cs
@@ -1,29 +1,30 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Net.Mime;
 using UnityEngine;
-using UnityEngine.Analytics;
 
 public class ObstacleGenerator : MonoBehaviour
 {
+    enum ObstacleType
+    {
+        obstacle, spike, doubleSpike, tripleSpike, trampolineSpike
+    }
+
     // PRUEBAS Graves Y Agudos
     [SerializeField] private GameObject badPrefab;
     [SerializeField] private GameObject waterPrefab;
     [SerializeField] private GameObject coinPrefab;
-
 
     [SerializeField] private GameObject features;
 
     // Prefabs
     [SerializeField] private GameObject groundPrefab;
     [SerializeField] private GameObject groundStartPrefab;
-    [SerializeField] private GameObject obstaclePrefab;
-    [SerializeField] private GameObject spikePrefab;
-    //[SerializeField] private GameObject doubleSpikePrefab;
-    //[SerializeField] private GameObject tripleSpikePrefab;
+    [SerializeField] private GameObject[] obstacles;
+
+    // Obstacle Array
+    private readonly ObstacleType[] allObstacles = {ObstacleType.obstacle, ObstacleType.spike,
+                                                    ObstacleType.doubleSpike, ObstacleType.tripleSpike,
+                                                    ObstacleType.trampolineSpike};
 
     // Pools
     [SerializeField] private Transform obstaclePool;
@@ -35,7 +36,7 @@ public class ObstacleGenerator : MonoBehaviour
 
     //public int BPM;
     //public float finalLevel;
-  
+
     void Start()
     {
         float width = transform.localScale.x;
@@ -60,37 +61,24 @@ public class ObstacleGenerator : MonoBehaviour
             {
                 for (int j = 0; j < 10; j++)
                     Instantiate(groundStartPrefab, new Vector3(x - width * j, y - height, 0), transform.rotation, groundPool);
-                Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
+                Instantiate(obstacles[(int)ObstacleType.obstacle], new Vector3(x, y, 0), transform.rotation, obstaclePool);
                 continue;
             }
             float prevX = beats[i - 1] * multiplierX;
+            float nextX = beats[i + 1] * multiplierX;
             int prevY = (int)(scopt[i - 1] * multiplierY);
             int nextY = (int)(scopt[i + 1] * multiplierY);
             float distance = x - prevX;
-            Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-            if (y - prevY == 3) Obstacle3(height, x, y, prevX, prevY, distance);
-            else if (nextY - y == 2) Obstacle2(height, x, y, prevX, distance);
-            if (y - prevY == 0 || y - prevY == 1)
+
+
+            Ground0_1(prevX, y, distance, width, height);
+
+            if (nextY - y == 0)
             {
-                Ground0_1(height, y, prevX, distance);
-                if (nextY - y == 1) Instantiate(spikePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-                else Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
+                InstantiateRandomObstacle(x, y);
             }
-            else if (y - prevY == 2)
-            {
-                Instantiate(obstaclePrefab, new Vector3((x + prevX) / 2, y - 1, 0), transform.rotation, obstaclePool);
-                Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-            }
-            else if (y - prevY == -1)
-            {
-                Ground0_1(height, y, prevX, distance);
-                Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-            }
-            else
-            {
-                Ground0_1(height, y, prevX, distance);
-                Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-            }
+            else Instantiate(obstacles[(int)ObstacleType.obstacle], new Vector3(x, y, 0), transform.rotation, obstaclePool);
+
         }
 
         #region pruebas GyA
@@ -120,31 +108,22 @@ public class ObstacleGenerator : MonoBehaviour
 
     private void Obstacle(float height, float x, int y, float prevX, float distance)
     {
-        float obstacleWidth = obstaclePrefab.transform.localScale.x / 2;
-        GameObject ground = Instantiate(groundPrefab, new Vector3(prevX + distance / 2 + obstacleWidth, y - height, 0), transform.rotation, groundPool);
-        ground.transform.localScale = new Vector3(distance, ground.transform.localScale.y, ground.transform.localScale.z);
-        Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
+        //float obstacleWidth = obstaclePrefab.transform.localScale.x / 2;
+        //GameObject ground = Instantiate(groundPrefab, new Vector3(prevX + distance / 2 + obstacleWidth, y - height, 0), transform.rotation, groundPool);
+        //ground.transform.localScale = new Vector3(distance, ground.transform.localScale.y, ground.transform.localScale.z);
+        //Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
     }
 
-    private void Ground0_1(float height, int y, float prevX, float distance)
+    private void Ground0_1(float prevX, int y, float distance, float width, float height)
     {
-        float obstacleWidth = obstaclePrefab.transform.localScale.x / 2;
-        GameObject ground = Instantiate(groundPrefab, new Vector3(prevX + distance / 2 + obstacleWidth, y - height, 0), transform.rotation, groundPool);
+        GameObject ground = Instantiate(groundPrefab, new Vector3(prevX + distance / 2 + width / 2, y - height, 0), transform.rotation, groundPool);
         ground.transform.localScale = new Vector3(distance, ground.transform.localScale.y, ground.transform.localScale.z);
     }
 
-    private void Obstacle2(float height, float x, int y, float prevX, float distance)
+    private void InstantiateRandomObstacle(float x, float y)
     {
-        GameObject ground = Instantiate(groundPrefab, new Vector3(distance / 2 + prevX + 1, y - height, 0), transform.rotation, groundPool);
-        ground.transform.localScale = new Vector3(distance - 1, ground.transform.localScale.y, ground.transform.localScale.z);
-        Instantiate(spikePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
-    }
-    private void Obstacle3(float height, float x, int y, float prevX, float prevY, float distance)
-    {
-        //Instantiate(obstaclePrefab, new Vector3(distance / 2 + prevX, (y + prevY) / 2, 0), transform.rotation, obstaclePool);
-        //Transform aux = groundPool.GetChild(groundPool.transform.childCount - 1);
-        //aux.localScale = new Vector3(2, 1, 1);
-        //aux.position = new Vector3(prevX + 1 + (aux.localScale.x / 2), y, 0);
+        int rnd = Random.Range(0, allObstacles.Length);
+        Instantiate(obstacles[(int)allObstacles[rnd]], new Vector2(x, y), transform.rotation, obstaclePool);
     }
 
     public float getMultiplierX()
