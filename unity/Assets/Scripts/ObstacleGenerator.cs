323385fb95a7b2ce3f22532c99b5092e765b0ded
rubenglezortiz <rubgon08@ucm.es>
2023-03-11 19:23:43 +0100

    interactiveTrampoline y obstáculos con diferencia de altura +3

--- unity/Assets/Scripts/ObstacleGenerator.cs
+++ unity/Assets/Scripts/ObstacleGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Analytics;
@@ -8,7 +9,7 @@ public class ObstacleGenerator : MonoBehaviour
 {
     enum ObstacleType
     {
-        obstacle, spike, doubleSpike, tripleSpike, trampolineSpike
+        obstacle, spike, doubleSpike, tripleSpike, trampolineSpike, trampoline, interactiveTrampoline
     }
 
     // PRUEBAS Graves Y Agudos
@@ -23,11 +24,12 @@ public class ObstacleGenerator : MonoBehaviour
     [SerializeField] private GameObject groundPrefab;
     [SerializeField] private GameObject groundStartPrefab;
     [SerializeField] private GameObject[] obstacles;
-    [SerializeField] private GameObject trampoline;
 
     // Obstacle Array
     private readonly ObstacleType[] allObstacles = {ObstacleType.obstacle, ObstacleType.spike,
                                                     ObstacleType.doubleSpike, ObstacleType.tripleSpike,
+                                                    ObstacleType.trampolineSpike};
+    private readonly ObstacleType[] centerObstacles = {ObstacleType.obstacle, ObstacleType.spike,
                                                     ObstacleType.trampolineSpike};
 
     // Pools
@@ -99,11 +101,12 @@ public class ObstacleGenerator : MonoBehaviour
     private void GenerateLevel(float width, float height, List<float> beats, List<float> scopt, List<int> beatsZonesIndex)
     {
         bool portal = false;
-        for (int i = 2; i < beats.Count() - 1; i++)
+        int offsetI = 2;
+        for (int i = offsetI; i < beats.Count() - 1; i++)
         {
             float x = beats[i] * multiplierX;
             int y = (int)(scopt[i] * multiplierY);
-            if (i == 2)
+            if (i == offsetI)
             {
                 Ground0_1(-1, y, x + 1, width, height);
                 continue;
@@ -132,19 +135,23 @@ public class ObstacleGenerator : MonoBehaviour
                 continue;
             }
 
+
+            if (y - prevY == 3)
+            {
+                InstantiateObstacle3Up(x, y, prevX, prevY);
+                continue;
+            }
             Ground0_1(prevX, y, distance, width, height);
             if (nextY - y == 0) InstantiateRandomObstacle(x, y);
+            else if (nextY - y == 2) InstantiateObstacle2Up(x, y);
             else Instantiate(obstacles[(int)ObstacleType.obstacle], new Vector3(x, y, 0), transform.rotation, obstaclePool);
         }
 
     }
 
-    private void Obstacle(float height, float x, int y, float prevX, float distance)
+    private void Obstacle(float x, int y, float prevX, int prevY)
     {
-        //float obstacleWidth = obstaclePrefab.transform.localScale.x / 2;
-        //GameObject ground = Instantiate(groundPrefab, new Vector3(prevX + distance / 2 + obstacleWidth, y - height, 0), transform.rotation, groundPool);
-        //ground.transform.localScale = new Vector3(distance, ground.transform.localScale.y, ground.transform.localScale.z);
-        //Instantiate(obstaclePrefab, new Vector3(x, y, 0), transform.rotation, obstaclePool);
+        Instantiate(obstacles[(int)ObstacleType.obstacle], new Vector3((x + prevX) / 2f, (y + prevY) / 2f, 0), transform.rotation, obstaclePool);
     }
 
     private void Ground0_1(float prevX, int y, float distance, float width, float height)
@@ -157,6 +164,19 @@ public class ObstacleGenerator : MonoBehaviour
     {
         int rnd = Random.Range(0, allObstacles.Length);
         Instantiate(obstacles[(int)allObstacles[rnd]], new Vector2(x, y), transform.rotation, obstaclePool);
+    }
+
+    private void InstantiateObstacle2Up(float x, int y)
+    {
+        Instantiate(obstacles[(int)ObstacleType.spike], new Vector2(x, y), transform.rotation, obstaclePool);
+    }
+
+    private void InstantiateObstacle3Up(float x, int y, float prevX, int prevY)
+    {
+        int rnd = Random.Range(0, centerObstacles.Length);
+        Destroy(obstaclePool.GetChild(obstaclePool.childCount - 1).gameObject);
+        Instantiate(obstacles[(int)ObstacleType.interactiveTrampoline], new Vector2((x + prevX) / 2f, (y + prevY) / 2f), transform.rotation, obstaclePool);
+        Instantiate(obstacles[(int)centerObstacles[rnd]], new Vector2(x, y), transform.rotation, obstaclePool);
     }
 
     public float getMultiplierX()
--- unity/Assets/Scripts/Trampoline.cs
+++ unity/Assets/Scripts/Trampoline.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    public float m;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool interactive;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerMovement mov = collision.gameObject.GetComponent<PlayerMovement>();
         if (mov != null)
         {
+            if (interactive && !(Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.Space))) return;
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * 26.6581f*m, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * 26.6581f * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
