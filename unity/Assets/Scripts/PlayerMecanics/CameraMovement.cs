38f996078ad570632f7f5af074bc42d7dab71039
rubenglezortiz <rubgon08@ucm.es>
2023-04-02 20:00:58 +0200

    movimiento de la cámara suavizado

--- unity/Assets/Scripts/PlayerMecanics/CameraMovement.cs
+++ unity/Assets/Scripts/PlayerMecanics/CameraMovement.cs
@@ -1,21 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform playerTr;
-    public float offsetX, offsetY;
-    float posY, posZ;
+    [SerializeField] private Transform playerTr;
+    [SerializeField] private float offsetX, offsetY;
+    float posZ;
 
     void Start()
     {
-        posY = transform.position.y;
         posZ = transform.position.z;
     }
 
     void Update()
     {
-        transform.position = new Vector3(playerTr.position.x + offsetX, /*posY*/playerTr.position.y + offsetY, posZ);
+        if (Mathf.Abs(transform.position.y - playerTr.position.y) > offsetY)
+            transform.position = new Vector3(playerTr.position.x + offsetX, Mathf.Lerp(transform.position.y, playerTr.position.y, Time.deltaTime), posZ);
+        else transform.position = new Vector3(playerTr.position.x + offsetX, transform.position.y, posZ);
     }
 }
